257262b0b2b74515ffb5c443137147bd1a5a2b0c
Muskanxydv <muskanyadav4095@gmail.com>
2024-11-15 14:40:30 +0530

    AJAX updated

--- script.js
+++ script.js
@@ -1,54 +1,81 @@
-document.getElementById("comment-bttn").addEventListener("click",function(){
-    const commentsection  = document.getElementById("comment-section");
+document.getElementById("comment-bttn").addEventListener("click", function () {
+    const commentsection = document.getElementById("comment-section");
     commentsection.classList.add("active");
 });
-document.querySelector(".cross").addEventListener("click",function(){
-    const commentsection  = document.getElementById("comment-section");
+
+document.querySelector(".cross").addEventListener("click", function () {
+    const commentsection = document.getElementById("comment-section");
     commentsection.classList.remove("active");
 });
 
 function autoResize(textarea) {
-    textarea.style.height = 'auto'; // Reset the height
-    textarea.style.height = textarea.scrollHeight + 'px';
+    textarea.style.height = "auto"; // Reset the height
+    textarea.style.height = textarea.scrollHeight + "px";
 }
-// This function will reload the comments from our local storage even after our page is reloaded
-window.addEventListener('load', function() { // It means when the page is reloaded the follwing function will be performed
-    const savedComments = JSON.parse(localStorage.getItem('comments')) || []; //It'll check the comments in our local storage and find 'em
-    savedComments.forEach(comment => { // and for each of the comment we find there
-        addCommentToList(comment.name, comment.comment, comment.timeStamp); // we will add the name, comment and time in the commen-section's commentList
+
+// Reload the comments from local storage when the page loads
+window.addEventListener("load", function () {
+    const savedComments = JSON.parse(localStorage.getItem("comments")) || []; // Check for comments in local storage
+    savedComments.forEach((comment) => {
+        addCommentToList(comment.name, comment.comment, comment.timeStamp); // Add each saved comment to the list
     });
 });
 
-document.getElementById('comment-section').addEventListener('submit', function(event) { //It means when our submit button is cliked the following function is performed
-    event.preventDefault(); // it means it'll not follow the default function when the button is clicked, i.e., to reload the page when we press submit (which basically just means it'll prevent page reload)
+// Handle the submission of a new comment
+document.getElementById("comment-section").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the page from reloading on form submission
 
-    const name = document.getElementById('name').value; //it'll find/get the name typed in the name field
-    const comment = document.getElementById('comment').value; //it'll find/get the comment typed in comment field
-    const timeStamp = new Date().toLocaleString(); //it'll add the "NEW(data)"(as mentioned in the name itself) which means it'll add a date and time string along with the comment and name so basically it just means it's the data other than what the user provided which we have to show
-    
-    addCommentToList(name, comment, timeStamp); // it'll add all the three data we got previously in the list we created in script.js 
+    const name = document.getElementById("name").value; // Get the name input
+    const comment = document.getElementById("comment").value; // Get the comment input
+    const timeStamp = new Date().toLocaleString(); // Generate a timestamp
 
-    saveCommentToLocalStorage(name, comment, timeStamp); //it'll add our each comment data the is posted into our local storage
+    addCommentToList(name, comment, timeStamp); // Display the new comment
+    saveCommentToLocalStorage(name, comment, timeStamp); // Save the new comment to local storage
 
-    document.getElementById('name').value = ''; // it clears the name typed in name input box after the comment is posted
-    document.getElementById('comment').value = ''; // it clears the comment typed in comment input box after the comment is posted
+    document.getElementById("name").value = ""; // Clear the name input
+    document.getElementById("comment").value = ""; // Clear the comment input
 });
 
-// This is basically just the function we used above to add the data to commentList
+// Add a comment to the comment list in the UI
 function addCommentToList(name, comment, timeStamp) {
-    const commentsList = document.getElementById('commentsList'); //it'll find the commentsList from index.html
-    const commentItem = document.createElement('li'); // it'll create a new item in the ul named "commentsList" we found in upper step
+    const commentsList = document.getElementById("commentsList");
+    const commentItem = document.createElement("li");
     commentItem.innerHTML = `
-        <strong>${name}</strong> <i>${timeStamp}</i> : <br> 
-        ${comment} 
-    `; // it just defines how the comment will be displayed like first the name in bold then time stamp in italics and the comment in the next line
-    commentsList.appendChild(commentItem); // it just means we're 'appending' the new comment to our already existing list(must've seen the diff b/w wrting and appending in file handling in python right... so it's basically the same appending
+        <strong>${name}</strong> <i>${timeStamp}</i>:<br>
+        ${comment}
+    `;
+    commentsList.appendChild(commentItem);
 }
 
-// This is basically just the function we used above to save the comment data in our local storage
+// Save a comment to local storage
 function saveCommentToLocalStorage(name, comment, timeStamp) {
     const newComment = { name, comment, timeStamp };
-    let comments = JSON.parse(localStorage.getItem('comments')) || []; // it checks if there are already the same items stored in local storge or not to avoid duplicating and if not it'll create a new array (in local storages the data is usually stored in arrays)
-    comments.push(newComment); // it'll add the new comment to the array we created in upper step, that is, if it's created
-    localStorage.setItem('comments', JSON.stringify(comments)); // Save updated comments to local storage
+    let comments = JSON.parse(localStorage.getItem("comments")) || []; // Get existing comments or initialize a new array
+    comments.push(newComment); // Add the new comment to the array
+    localStorage.setItem("comments", JSON.stringify(comments)); // Save the updated array to local storage
 }
+
+// Simulating AJAX-like local loading
+function fetchComments() {
+    // Simulate fetching from a server (in this case, local storage)
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const savedComments = JSON.parse(localStorage.getItem("comments")) || [];
+            resolve(savedComments);
+        }, 500); // Simulating a 500ms delay for AJAX
+    });
+}
+
+// Load comments using "AJAX" (local storage fetch)
+function loadCommentsWithAJAX() {
+    fetchComments().then((comments) => {
+        document.getElementById("commentsList").innerHTML = ""; // Clear existing comments
+        comments.forEach((comment) => {
+            addCommentToList(comment.name, comment.comment, comment.timeStamp);
+        });
+    });
+}
+
+// Trigger the AJAX-like loading on page load
+window.addEventListener("load", loadCommentsWithAJAX);
+
